0b2231272693438ecfe99fc83f7a0a6373f25c09
matt-henriksen <73131815+matt-henriksen@users.noreply.github.com>
2022-12-07 16:38:45 -0500

    Updated Routing For Two Pages
    Routing is updated for the two pages:
    - Login Page (OAuth)
    - Home Page (Main Content)

--- Mx-Worldwide/src/app/app.module.ts
+++ Mx-Worldwide/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
-import { PlayerComponent } from './player';
 
 
 @NgModule({
@@ -21,7 +20,6 @@ import { PlayerComponent } from './player';
       AppComponent,
       HomeComponent,
       LoginComponent,
-      PlayerComponent,
   ],
   bootstrap: [AppComponent]
 })
--- Mx-Worldwide/src/app/player/player.component.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({ templateUrl: 'player.component.html' })
-export class PlayerComponent {}
